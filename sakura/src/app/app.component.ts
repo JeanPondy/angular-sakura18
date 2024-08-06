34d4243a41d21d888ede575bcc4e0c4154115d49
Jean Pondy <manbayesse@gmail.com>
2024-08-06 05:50:56 +0200

    Erste component erstellen

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LandungPageComponent } from './landingPage/landingPage.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandungPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
